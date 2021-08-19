cd2772dadc98075ce14fa74ceb08eb16f77ec90c
Woody230 <bselzer1@outlook.com>
2021-08-18 22:03:48 -0400

    Boolean extensions

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 
 allprojects {
     group = "com.bselzer.library.kotlin.extension"
-    version = "1.2.9"
+    version = "1.2.10"
 
     apply(plugin = "maven-publish")
 
