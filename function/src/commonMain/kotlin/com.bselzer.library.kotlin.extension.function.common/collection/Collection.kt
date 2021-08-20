50774d18bd6c914b4a0beced77012d6ea2758fdb
Woody230 <bselzer1@outlook.com>
2021-08-20 07:09:46 -0400

    Fill extension

--- function/src/commonMain/kotlin/com.bselzer.library.kotlin.extension.function.common/collection/Collection.kt
+++ function/src/commonMain/kotlin/com.bselzer.library.kotlin.extension.function.common/collection/Collection.kt
@@ -11,3 +11,19 @@ fun <T> Collection<T>.plusElementIf(item: T, block: T.() -> Boolean): Collection
         else -> this
     }
 }
+
+/**
+ * Fills the collection with new objects from the [create] block in order to meet the [capacity].
+ * @return the collection
+ */
+fun <T> Collection<T>.fill(capacity: Int, create: () -> T): Collection<T> = apply {
+    val collection = this.toMutableList()
+
+    // Fill the remaining space if the items do not exist.
+    while (collection.size < capacity)
+    {
+        collection.add(create())
+    }
+
+    return collection
+}
