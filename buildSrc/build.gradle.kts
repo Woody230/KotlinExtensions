f7e55595cc4e0f1f96fe22a3fdfa92a6ff0c01cc
Woody230 <bselzer1@outlook.com>
2021-10-10 23:03:20 -0400

    Update android build tool version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,5 +10,5 @@ repositories {
 
 dependencies {
     implementation(kotlin("gradle-plugin:1.5.30"))
-    implementation("com.android.tools.build:gradle:7.0.0")
+    implementation("com.android.tools.build:gradle:7.0.2")
 }
