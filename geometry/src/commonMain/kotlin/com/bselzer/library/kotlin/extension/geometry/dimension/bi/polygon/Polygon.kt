325c79c7aea7650184e612038cb39f09221beebc
Woody230 <bselzer1@outlook.com>
2021-10-17 06:43:20 -0400

    Cleanup

--- geometry/src/commonMain/kotlin/com/bselzer/library/kotlin/extension/geometry/dimension/bi/polygon/Polygon.kt
+++ geometry/src/commonMain/kotlin/com/bselzer/library/kotlin/extension/geometry/dimension/bi/polygon/Polygon.kt
@@ -7,11 +7,4 @@ abstract class Polygon(
      * The points forming the shape.
      */
     internal val points: List<Point>
-) {
-    internal companion object {
-        /**
-         * @return the point at the [index] or a default point
-         */
-        fun List<Point>.extract(index: Int): Point = getOrElse(index) { Point() }
-    }
-}
+)
