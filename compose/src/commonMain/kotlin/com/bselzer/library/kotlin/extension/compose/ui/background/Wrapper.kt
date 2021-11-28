2f3c198509deadb28e87329678ed0e93feb63b94
Woody230 <bselzer1@outlook.com>
2021-11-28 10:32:01 -0500

    Add content modifier to BackgroundRow/Column

--- compose/src/commonMain/kotlin/com/bselzer/library/kotlin/extension/compose/ui/background/Wrapper.kt
+++ compose/src/commonMain/kotlin/com/bselzer/library/kotlin/extension/compose/ui/background/Wrapper.kt
@@ -27,9 +27,10 @@ fun Background(
 /**
  * Wraps the [background] in a [Box] and displays the [content] in a [Row].
  *
- * @param modifier the row modifier
+ * @param modifier the [Box] modifier
  * @param alignment the alignment of the [background] and [Row]
  * @param background the block for displaying the background
+ * @param contentModifier the [Row] modifier
  * @param contentHorizontalArrangement the horizontal arrangement of the [content]
  * @param contentVerticalAlignment the vertical alignment of the [content]
  * @param content the block for displaying the content overlaying the background
@@ -39,19 +40,28 @@ fun BackgroundRow(
     modifier: Modifier = Modifier,
     alignment: Alignment = Alignment.TopStart,
     background: @Composable BoxScope.() -> Unit,
+    contentModifier: Modifier = Modifier,
     contentHorizontalArrangement: Arrangement.Horizontal = Arrangement.Start,
     contentVerticalAlignment: Alignment.Vertical = Alignment.Top,
     content: @Composable RowScope.() -> Unit
 ) = Background(modifier = modifier, contentAlignment = alignment, background = background) {
-    Row(modifier = Modifier.wrapContentSize(), horizontalArrangement = contentHorizontalArrangement, verticalAlignment = contentVerticalAlignment, content = content)
+    Row(
+        modifier = Modifier
+            .wrapContentSize()
+            .then(contentModifier),
+        horizontalArrangement = contentHorizontalArrangement,
+        verticalAlignment = contentVerticalAlignment,
+        content = content
+    )
 }
 
 /**
  * Wraps the [background] in a [Box] and displays the [content] in a [Column].
  *
- * @param modifier the column modifier
+ * @param modifier the [Box] modifier
  * @param alignment the alignment of the [background] and [Column]
  * @param background the block for displaying the background
+ * @param contentModifier the [Column] modifier
  * @param contentVerticalArrangement the vertical arrangement of the [content]
  * @param contentHorizontalAlignment the horizontal alignment of the [content]
  * @param content the block for displaying the content overlaying the background
@@ -61,9 +71,17 @@ fun BackgroundColumn(
     modifier: Modifier = Modifier,
     alignment: Alignment = Alignment.TopStart,
     background: @Composable BoxScope.() -> Unit,
+    contentModifier: Modifier = Modifier,
     contentVerticalArrangement: Arrangement.Vertical = Arrangement.Top,
     contentHorizontalAlignment: Alignment.Horizontal = Alignment.Start,
     content: @Composable ColumnScope.() -> Unit
 ) = Background(modifier = modifier, contentAlignment = alignment, background = background) {
-    Column(modifier = Modifier.wrapContentSize(), verticalArrangement = contentVerticalArrangement, horizontalAlignment = contentHorizontalAlignment, content = content)
+    Column(
+        modifier = Modifier
+            .wrapContentSize()
+            .then(contentModifier),
+        verticalArrangement = contentVerticalArrangement,
+        horizontalAlignment = contentHorizontalAlignment,
+        content = content
+    )
 }
