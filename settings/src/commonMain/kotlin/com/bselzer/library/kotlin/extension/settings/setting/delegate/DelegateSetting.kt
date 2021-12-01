415e01328eaae3dd06a3e0957b31fbcb95e40631
Woody230 <bselzer1@outlook.com>
2021-11-30 21:48:49 -0500

    Delegate setting => Setting interface

--- settings/src/commonMain/kotlin/com/bselzer/library/kotlin/extension/settings/setting/delegate/DelegateSetting.kt
+++ settings/src/commonMain/kotlin/com/bselzer/library/kotlin/extension/settings/setting/delegate/DelegateSetting.kt
@@ -1,16 +1,18 @@
 package com.bselzer.library.kotlin.extension.settings.setting.delegate
 
 import com.bselzer.library.kotlin.extension.settings.setting.Setting
-import com.bselzer.library.kotlin.extension.settings.setting.encapsulation.*
 import kotlinx.coroutines.flow.Flow
 import kotlin.properties.ReadWriteProperty
 
 /**
  * The base class for delegating getting/setting a [Setting]
  */
-abstract class DelegateSetting<T>(protected val wrapper: Setting<T>) : ReadWriteProperty<Any?, T?>, Exists, Initialize<T>, Removable, SafeObserver<T>, NullObserver<T?> {
-    val key = wrapper.key
-    val defaultValue = wrapper.defaultValue
+abstract class DelegateSetting<T>(protected val wrapper: Setting<T>) : ReadWriteProperty<Any?, T?>, Setting<T> {
+    override val key = wrapper.key
+    override val defaultValue = wrapper.defaultValue
+    override fun get(): T = wrapper.get()
+    override fun getOrNull(): T? = wrapper.getOrNull()
+    override fun put(value: T) = wrapper.put(value)
     override fun exists(): Boolean = wrapper.exists()
     override fun initialize(value: T): Boolean = wrapper.initialize(value)
     override fun remove() = wrapper.remove()
