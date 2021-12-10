7b6a9a4789d09f2870725e5ee6d6dd0e8f0e941b
Woody230 <bselzer1@outlook.com>
2021-12-10 14:09:22 -0500

    Dividers with prepend/append boolean parameter

--- compose/src/commonMain/kotlin/com/bselzer/library/kotlin/extension/compose/ui/container/Divider.kt
+++ compose/src/commonMain/kotlin/com/bselzer/library/kotlin/extension/compose/ui/container/Divider.kt
@@ -21,8 +21,8 @@ fun DividedColumn(
     modifier: Modifier = Modifier,
     verticalArrangement: Arrangement.Vertical = Arrangement.Top,
     horizontalAlignment: Alignment.Horizontal = Alignment.Start,
-    prepend: @Composable ColumnScope.() -> Unit = { },
-    append: @Composable ColumnScope.() -> Unit = { },
+    prepend: @Composable ColumnScope.() -> Unit,
+    append: @Composable ColumnScope.() -> Unit,
     divider: @Composable ColumnScope.(Int) -> Unit,
     vararg contents: @Composable ColumnScope.() -> Unit
 ) = Column(modifier = modifier, verticalArrangement = verticalArrangement, horizontalAlignment = horizontalAlignment) {
@@ -40,6 +40,36 @@ fun DividedColumn(
 }
 
 /**
+ * Wraps a [Column] with the [divider] if [prepend] is true, the [contents] separated by the [divider], and then the [divider] if [append] is true.
+ *
+ * @param modifier the [Column] modifier
+ * @param verticalArrangement the vertical arrangement of the content
+ * @param horizontalAlignment the horizontal alignment of the content
+ * @param prepend whether to lay out the [divider] before the main [contents] is displayed
+ * @param append whether to lay out the [divider] after the main [contents] is displayed
+ * @param divider the divider between the [contents], except for between the column and the first and last items which should be indicated by [prepend] and [append] respectively instead
+ * @param contents the main content
+ */
+@Composable
+fun DividedColumn(
+    modifier: Modifier = Modifier,
+    verticalArrangement: Arrangement.Vertical = Arrangement.Top,
+    horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+    prepend: Boolean = false,
+    append: Boolean = false,
+    divider: @Composable ColumnScope.() -> Unit,
+    vararg contents: @Composable ColumnScope.() -> Unit
+) = DividedColumn(
+    modifier = modifier,
+    verticalArrangement = verticalArrangement,
+    horizontalAlignment = horizontalAlignment,
+    prepend = { if (prepend) divider() },
+    append = { if (append) divider() },
+    divider = { divider() },
+    contents = contents
+)
+
+/**
  * Wraps a [Row] with the [prepend] block, the [contents] separated by the [divider], and then the [append] block.
  *
  * @param modifier the [Row] modifier
@@ -55,8 +85,8 @@ fun DividedRow(
     modifier: Modifier = Modifier,
     horizontalArrangement: Arrangement.Horizontal = Arrangement.Start,
     verticalAlignment: Alignment.Vertical = Alignment.Top,
-    prepend: @Composable RowScope.() -> Unit = { },
-    append: @Composable RowScope.() -> Unit = { },
+    prepend: @Composable RowScope.() -> Unit,
+    append: @Composable RowScope.() -> Unit,
     divider: @Composable RowScope.(Int) -> Unit,
     vararg contents: @Composable RowScope.() -> Unit
 ) = Row(modifier = modifier, horizontalArrangement = horizontalArrangement, verticalAlignment = verticalAlignment) {
@@ -72,3 +102,34 @@ fun DividedRow(
 
     append()
 }
+
+
+/**
+ * Wraps a [Row] with the [divider] if [prepend] is true, the [contents] separated by the [divider], and then the [divider] if [append] is true.
+ *
+ * @param modifier the [Row] modifier
+ * @param horizontalArrangement the horizontal arrangement of the content
+ * @param verticalAlignment the vertical alignment of the content
+ * @param prepend whether to lay out the [divider] before the main [contents] is displayed
+ * @param append whether to lay out the [divider] after the main [contents] is displayed
+ * @param divider the divider between the [contents], except for between the column and the first and last items which should be indicated by [prepend] and [append] respectively instead
+ * @param contents the main content
+ */
+@Composable
+fun DividedRow(
+    modifier: Modifier = Modifier,
+    horizontalArrangement: Arrangement.Horizontal = Arrangement.Start,
+    verticalAlignment: Alignment.Vertical = Alignment.Top,
+    prepend: Boolean = false,
+    append: Boolean = false,
+    divider: @Composable RowScope.() -> Unit,
+    vararg contents: @Composable RowScope.() -> Unit
+) = DividedRow(
+    modifier = modifier,
+    horizontalArrangement = horizontalArrangement,
+    verticalAlignment = verticalAlignment,
+    prepend = { if (prepend) divider() },
+    append = { if (append) divider() },
+    divider = { divider() },
+    contents = contents
+)
