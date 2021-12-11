6958e9d132d5a3d849b0c80134bc63b880b1a50f
Woody230 <bselzer1@outlook.com>
2021-12-11 13:57:53 -0500

    Delete button

--- compose/src/commonMain/kotlin/com/bselzer/library/kotlin/extension/compose/ui/appbar/Icon.kt
+++ compose/src/commonMain/kotlin/com/bselzer/library/kotlin/extension/compose/ui/appbar/Icon.kt
@@ -3,6 +3,7 @@ package com.bselzer.library.kotlin.extension.compose.ui.appbar
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
@@ -19,5 +20,19 @@ fun RefreshIcon(onClick: suspend CoroutineScope.() -> Unit) {
     val scope = rememberCoroutineScope()
     IconButton(onClick = { scope.launch { onClick(scope) } }) {
         Icon(Icons.Filled.Refresh, contentDescription = "Refresh")
+    }
+}
+
+/**
+ * Displays a delete icon for an app bar.
+ *
+ * @param enabled whether the on-click handler is enabled
+ * @param onClick the on-click handler
+ */
+@Composable
+fun DeleteButton(enabled: Boolean = true, onClick: suspend CoroutineScope.() -> Unit) {
+    val scope = rememberCoroutineScope()
+    IconButton(enabled = enabled, onClick = { scope.launch { onClick(scope) } }) {
+        Icon(Icons.Filled.Delete, contentDescription = "Delete")
     }
 }
