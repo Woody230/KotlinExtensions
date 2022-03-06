e140736e3e5defe4bf7ae7a17fcbe244dc07ef97
Woody230 <bselzer1@outlook.com>
2022-03-06 14:47:26 -0500

    Fix build

---
+++ compose-resource/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/dialog/DismissButton.kt
@@ -0,0 +1,26 @@
+package com.bselzer.ktx.compose.ui.dialog
+
+import androidx.compose.material.Button
+import androidx.compose.runtime.Composable
+import com.bselzer.ktx.compose.ui.style.*
+import com.bselzer.ktx.resource.Resources
+import dev.icerock.moko.resources.compose.stringResource
+
+/**
+ * Lays out a button for dismissing a dialog.
+ *
+ * @param style the style of the [Button]
+ * @param textStyle the style of the text
+ * @param onClick the on-click handler
+ */
+@Composable
+fun DismissButton(
+    style: ButtonStyle = LocalButtonStyle.localized(),
+    textStyle: WordStyle = LocalWordStyle.localized(),
+    onClick: () -> Unit
+) = MaterialDialogButton(
+    text = stringResource(Resources.strings.cancel),
+    textStyle = textStyle,
+    style = style,
+    onClick = onClick
+)
---
+++ compose-resource/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/dialog/ResetButton.kt
@@ -0,0 +1,21 @@
+package com.bselzer.ktx.compose.ui.dialog
+
+import androidx.compose.material.Button
+import androidx.compose.runtime.Composable
+import com.bselzer.ktx.compose.ui.style.*
+import com.bselzer.ktx.resource.Resources
+import dev.icerock.moko.resources.compose.stringResource
+
+/**
+ * Lays out a button for deleting a selection.
+ *
+ * @param style the style of the [Button]
+ * @param textStyle the style of the text
+ * @param onClick the on-click handler
+ */
+@Composable
+fun ResetButton(
+    style: ButtonStyle = LocalButtonStyle.localized(),
+    textStyle: WordStyle = LocalWordStyle.localized(),
+    onClick: () -> Unit
+) = MaterialDialogButton(text = stringResource(Resources.strings.reset), textStyle = textStyle, style = style, onClick = onClick)
