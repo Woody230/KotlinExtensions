b9c3fa3f32c74369417d5986f3d011b7d6513709
Woody230 <bselzer1@outlook.com>
2022-04-15 07:07:50 -0400

    Safe merge use referential equality

--- function/src/commonMain/kotlin/com/bselzer/ktx/function/objects/Object.kt
+++ function/src/commonMain/kotlin/com/bselzer/ktx/function/objects/Object.kt
@@ -3,7 +3,7 @@ package com.bselzer.ktx.function.objects
 /**
  * Merges this object with the other object by taking the [other] object if it is not the [default], otherwise taking this object.
  */
-fun <T> T.safeMerge(other: T, default: T): T = if (other != default) other else this
+fun <T> T.safeMerge(other: T, default: T): T = if (other !== default) other else this
 
 /**
  * Merges this object with the other object by taking the [other] object if it is not null, otherwise taking this object.
