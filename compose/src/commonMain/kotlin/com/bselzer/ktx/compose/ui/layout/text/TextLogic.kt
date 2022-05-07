ab2d896d4376b558f866ed8ed652060dfcba3e8d
Woody230 <bselzer1@outlook.com>
2022-05-06 20:39:46 -0400

    Missing comments

--- compose/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/layout/text/TextLogic.kt
+++ compose/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/layout/text/TextLogic.kt
@@ -6,9 +6,21 @@ import androidx.compose.ui.text.TextLayoutResult
 import com.bselzer.ktx.compose.ui.layout.project.LogicModel
 
 data class TextLogic(
+    /**
+     * The text to be displayed.
+     */
     val text: AnnotatedString,
+
+    /**
+     * Callback that is executed when a new text layout is calculated.
+     * A TextLayoutResult object that callback provides contains paragraph information, size of the text, baselines and other details.
+     * The callback can be used to add additional decoration or functionality to the text. For example, to draw selection around the text.
+     */
     val onTextLayout: (TextLayoutResult) -> Unit = {},
+
+    /**
+     * A map store composables that replaces certain ranges of the text.
+     * It's used to insert composables into text layout. Check InlineTextContent for more information.
+     */
     val inlineContent: Map<String, InlineTextContent> = mapOf()
-) : LogicModel {
-    constructor(text: String) : this(AnnotatedString(text))
-}
+) : LogicModel
