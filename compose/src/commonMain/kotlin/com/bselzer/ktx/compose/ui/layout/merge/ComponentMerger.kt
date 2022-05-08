789583d2801e7091adfb987fa3762c74293830c0
Woody230 <bselzer1@outlook.com>
2022-05-08 06:41:47 -0400

    Minor

--- compose/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/layout/merge/ComponentMerger.kt
+++ compose/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/layout/merge/ComponentMerger.kt
@@ -1,7 +1,6 @@
 package com.bselzer.ktx.compose.ui.layout.merge
 
 import com.bselzer.ktx.function.objects.safeMerge
-import com.bselzer.ktx.function.objects.safeTake
 
 interface ComponentMerger<T> {
     /**
@@ -23,14 +22,4 @@ interface ComponentMerger<T> {
      * Merges the [first] object with the [second] object by taking the [second] object if it is not the [default], otherwise taking the [first] object.
      */
     fun nullMerge(first: T?, second: T?) = first.safeMerge(second, default)
-
-    /**
-     * Takes the [first] object if it is not the [default], otherwise takes this object.
-     */
-    fun safeTake(first: T, second: T) = first.safeTake(second, default)
-
-    /**
-     * Takes the [first] object if it is not the [default], otherwise takes this object.
-     */
-    fun nullTake(first: T?, second: T?) = first.safeTake(second, default)
 }
