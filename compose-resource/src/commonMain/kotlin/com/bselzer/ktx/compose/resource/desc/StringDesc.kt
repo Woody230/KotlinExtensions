02ad1065eaaa5b22f8542e6fbb7a9ab4d674fb6f
Woody230 <bselzer1@outlook.com>
2022-05-16 21:08:55 -0400

    Temp localized

---
+++ compose-resource/src/commonMain/kotlin/com/bselzer/ktx/compose/resource/desc/StringDesc.kt
@@ -0,0 +1,9 @@
+package com.bselzer.ktx.compose.resource.desc
+
+import androidx.compose.runtime.Composable
+import dev.icerock.moko.resources.compose.localized
+import dev.icerock.moko.resources.desc.StringDesc
+
+// TODO temporary localized() reference
+@Composable
+fun StringDesc.localized() = localized()
