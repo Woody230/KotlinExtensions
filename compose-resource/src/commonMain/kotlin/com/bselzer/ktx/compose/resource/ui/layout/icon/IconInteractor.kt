6c700db6987aaeffaf8f54c221739ba246f7b958
Woody230 <bselzer1@outlook.com>
2022-05-19 09:47:57 -0400

    Add checkbox icons

--- compose-resource/src/commonMain/kotlin/com/bselzer/ktx/compose/resource/ui/layout/icon/IconInteractor.kt
+++ compose-resource/src/commonMain/kotlin/com/bselzer/ktx/compose/resource/ui/layout/icon/IconInteractor.kt
@@ -3,6 +3,7 @@ package com.bselzer.ktx.compose.resource.ui.layout.icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.state.ToggleableState
 import com.bselzer.ktx.compose.resource.images.painter
 import com.bselzer.ktx.compose.ui.layout.icon.IconInteractor
 import com.bselzer.ktx.resource.Resources
@@ -70,7 +71,7 @@ fun dropdownIconInteractor() = IconInteractor(
 @Composable
 fun expansionIconInteractor(isExpanded: Boolean) = IconInteractor(
     painter = if (isExpanded) Icons.Filled.KeyboardArrowUp.painter() else Icons.Filled.KeyboardArrowDown.painter(),
-    contentDescription = stringResource(resource = if (isExpanded) Resources.strings.condense else Resources.strings.expand),
+    contentDescription = stringResource(resource = if (isExpanded) Resources.strings.expand else Resources.strings.condense),
 )
 
 /**
@@ -98,4 +99,77 @@ fun drawerNavigationIconInteractor() = IconInteractor(
 fun refreshIconInteractor() = IconInteractor(
     painter = Icons.Filled.Refresh.painter(),
     contentDescription = stringResource(resource = Resources.strings.refresh),
+)
+
+/**
+ * Creates an interactor for indicating the use of a cache.
+ */
+@Composable
+fun cachedIconInteractor() = IconInteractor(
+    painter = Resources.images.ic_cached.painter(),
+    contentDescription = stringResource(Resources.strings.cache)
+)
+
+/**
+ * Creates an interactor for indicating the use of settings.
+ */
+@Composable
+fun settingsIconInteractor() = IconInteractor(
+    painter = Resources.images.ic_settings.painter(),
+    contentDescription = stringResource(Resources.strings.settings)
+)
+
+/**
+ * Creates an interactor for indicating the zooming in of a map.
+ */
+@Composable
+fun zoomInMapIconInteractor() = IconInteractor(
+    painter = Resources.images.ic_zoom_in_map.painter(),
+    contentDescription = stringResource(Resources.strings.zoom_in)
+)
+
+/**
+ * Creates an interactor for indicating the zooming out of a map.
+ */
+@Composable
+fun zoomOutMapIconInteractor() = IconInteractor(
+    painter = Resources.images.ic_zoom_out_map.painter(),
+    contentDescription = stringResource(Resources.strings.zoom_out)
+)
+
+/**
+ * Creates an interactor for indicating the use of content licenses.
+ */
+@Composable
+fun licenseIconInteractor() = IconInteractor(
+    painter = Resources.images.ic_policy.painter(),
+    contentDescription = stringResource(Resources.strings.licenses)
+)
+
+/**
+ * Creates an interactor for indicating information about the application.
+ */
+@Composable
+fun aboutAppIconInteractor() = IconInteractor(
+    painter = Resources.images.ic_info.painter(),
+    contentDescription = stringResource(Resources.strings.about_app)
+)
+
+/**
+ * Creates an interactor for indicating the on, off, or indeterminate state of a tri-state checkbox.
+ */
+@Composable
+fun triStateCheckboxIconInteractor(state: ToggleableState) = IconInteractor(
+    painter = when (state) {
+        ToggleableState.Indeterminate -> Resources.images.ic_checkbox_indeterminate
+        ToggleableState.Off -> Resources.images.ic_checkbox_outline_blank
+        ToggleableState.On -> Resources.images.ic_checkbox
+    }.painter(),
+    contentDescription = stringResource(
+        when (state) {
+            ToggleableState.Indeterminate -> Resources.strings.partially_selected
+            ToggleableState.Off -> Resources.strings.not_selected
+            ToggleableState.On -> Resources.strings.selected
+        }
+    )
 )
