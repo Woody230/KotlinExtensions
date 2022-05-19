38c0829f059f459e4f3536203b71a205c7180983
Woody230 <bselzer1@outlook.com>
2022-05-19 10:32:10 -0400

    Remove ShowSnackbar

--- compose/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/notification/snackbar/Snackbar.kt
+++ compose/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/notification/snackbar/Snackbar.kt
@@ -1,9 +1,6 @@
 package com.bselzer.ktx.compose.ui.notification.snackbar
 
-import androidx.compose.material.SnackbarDuration
 import androidx.compose.material.SnackbarHostState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.compositionLocalOf
 
@@ -11,18 +8,3 @@ import androidx.compose.runtime.compositionLocalOf
  * CompositionLocal containing the [SnackbarHostState] for sending notifications.
  */
 val LocalSnackbarHostState: ProvidableCompositionLocal<SnackbarHostState> = compositionLocalOf { SnackbarHostState() }
-
-/**
- * Displays a snackbar notification with the given [message] and button [actionLabel] for the given [duration].
- */
-@Composable
-fun ShowSnackbar(
-    message: String,
-    actionLabel: String? = null,
-    duration: SnackbarDuration = SnackbarDuration.Short,
-) {
-    val state = LocalSnackbarHostState.current
-    LaunchedEffect(message) {
-        state.showSnackbar(message, actionLabel, duration)
-    }
-}
