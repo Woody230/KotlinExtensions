017fffd19544d675001e7e2cda5cecab00eeeb65
Woody230 <bselzer1@outlook.com>
2022-05-20 18:42:28 -0400

    Use clickable text for offset

--- compose/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/layout/text/TextProjector.kt
+++ compose/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/layout/text/TextProjector.kt
@@ -1,14 +1,9 @@
 package com.bselzer.ktx.compose.ui.layout.text
 
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.style.TextAlign
 import com.bselzer.ktx.compose.ui.layout.project.Projector
 
@@ -21,43 +16,38 @@ class TextProjector(
         modifier: Modifier = Modifier,
         textAlign: TextAlign? = null
     ) = contextualize(modifier) { combinedModifier, interactor, presenter ->
-        val layoutResult = remember { mutableStateOf<TextLayoutResult?>(null) }
-        Text(
-            text = interactor.text,
-            color = presenter.color,
-            fontSize = presenter.fontSize,
-            fontStyle = presenter.fontStyle,
-            fontWeight = presenter.fontWeight,
-            fontFamily = presenter.fontFamily,
-            letterSpacing = presenter.letterSpacing,
-            textDecoration = presenter.textDecoration,
-            textAlign = textAlign ?: presenter.textAlign,
-            lineHeight = presenter.lineHeight,
-            overflow = presenter.overflow,
-            softWrap = presenter.softWrap.toBoolean(),
-            maxLines = presenter.maxLines,
-            inlineContent = interactor.inlineContent,
-            onTextLayout = { result ->
-                layoutResult.value = result
-                interactor.onTextLayout(result)
-            },
-            style = presenter.textStyle,
-            modifier = combinedModifier then pointerInputModifier(interactor, layoutResult.value)
-        )
-    }
-
-    /**
-     * Applies the [pointerInput] modifier if the onClickOffset handler exists, mimicking a [ClickableText].
-     */
-    private fun pointerInputModifier(interactor: TextInteractor, layoutResult: TextLayoutResult?) = if (interactor.onClickOffset == null) {
-        Modifier
-    } else {
-        Modifier.pointerInput(interactor.onClickOffset) {
-            detectTapGestures { position ->
-                layoutResult?.let { layoutResult ->
-                    interactor.onClickOffset.invoke(layoutResult.getOffsetForPosition(position), layoutResult.layoutInput.text)
-                }
-            }
+        if (interactor.onClickOffset == null) {
+            Text(
+                text = interactor.text,
+                color = presenter.color,
+                fontSize = presenter.fontSize,
+                fontStyle = presenter.fontStyle,
+                fontWeight = presenter.fontWeight,
+                fontFamily = presenter.fontFamily,
+                letterSpacing = presenter.letterSpacing,
+                textDecoration = presenter.textDecoration,
+                textAlign = textAlign ?: presenter.textAlign,
+                lineHeight = presenter.lineHeight,
+                overflow = presenter.overflow,
+                softWrap = presenter.softWrap.toBoolean(),
+                maxLines = presenter.maxLines,
+                inlineContent = interactor.inlineContent,
+                onTextLayout = interactor.onTextLayout,
+                style = presenter.textStyle,
+                modifier = combinedModifier
+            )
+        } else {
+            // TODO attempting to replicate the same logic over Text doesn't work?
+            ClickableText(
+                text = interactor.text,
+                modifier = combinedModifier,
+                style = presenter.textStyle,
+                softWrap = presenter.softWrap.toBoolean(),
+                overflow = presenter.overflow,
+                maxLines = presenter.maxLines,
+                onTextLayout = interactor.onTextLayout,
+                onClick = { offset -> interactor.onClickOffset.invoke(offset, interactor.text) }
+            )
         }
     }
 }
