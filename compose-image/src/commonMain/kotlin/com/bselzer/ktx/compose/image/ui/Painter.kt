3312a964379b0b6b5e9e7926a9a58fa8a7f2f22f
Woody230 <bselzer1@outlook.com>
2022-05-24 07:05:53 -0400

    Remove transaction/cache usage from rememberImagePainter

--- compose-image/src/commonMain/kotlin/com/bselzer/ktx/compose/image/ui/Painter.kt
+++ compose-image/src/commonMain/kotlin/com/bselzer/ktx/compose/image/ui/Painter.kt
@@ -1,16 +1,12 @@
 package com.bselzer.ktx.compose.image.ui
 
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.unit.IntOffset
-import com.bselzer.ktx.compose.image.cache.instance.ImageCache
 import com.bselzer.ktx.compose.image.model.Image
-import com.bselzer.ktx.kodein.db.transaction.Transaction
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 /**
  * Converts a [ByteArray] into an [ImageBitmap].
@@ -18,41 +14,25 @@ import kotlinx.coroutines.withContext
 internal expect fun ByteArray.asImageBitmap(): ImageBitmap
 
 /**
- * The cache for retrieving images.
- */
-val LocalImageCache: ProvidableCompositionLocal<ImageCache> = staticCompositionLocalOf { ImageCache() }
-
-/**
- * Creates a painter with the byte content from the image at the given [url].
+ * Creates a painter with the byte content from the [image].
  *
- * @param cache the image cache
- * @param url the url to retrieve the image from
+ * @param image the image to create a painter for
  * @param offset the subsection of the image to draw
  * @param filterQuality the sampling algorithm applied to the image when it is scaled
  */
 @Composable
-fun Transaction.rememberImagePainter(
-    cache: ImageCache = LocalImageCache.current,
-    url: String?,
+fun rememberImagePainter(
+    image: Image?,
     offset: IntOffset = IntOffset.Zero,
     filterQuality: FilterQuality = FilterQuality.Low,
 ): Painter? {
-    val image: Image? by produceState<Image?>(initialValue = null, key1 = url) {
-        value = url?.let {
-            withContext(Dispatchers.Default) {
-                val image = with(cache) { getImage(url) }
-
-                // Ignore defaulted images (no bytes).
-                return@withContext if (image.content.isEmpty()) null else image
-            }
-        }
-    }
+    val content = if (image?.content?.isNotEmpty() == true) image.content else null
 
     // Attempt to convert the image bytes into a bitmap.
-    val bitmap = image?.let {
+    val bitmap = content?.let {
         try {
             // TODO transformations
-            it.content.asImageBitmap()
+            content.asImageBitmap()
         } catch (ex: Exception) {
             null
         }
