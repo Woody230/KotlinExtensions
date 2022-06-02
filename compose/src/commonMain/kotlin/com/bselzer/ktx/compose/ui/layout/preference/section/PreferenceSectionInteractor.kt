02a8eebdeddcad58c1c5ca35c8669d2d20fc8cbc
Woody230 <bselzer1@outlook.com>
2022-06-01 21:46:37 -0400

    Update PreferenceSection

--- compose/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/layout/preference/section/PreferenceSectionInteractor.kt
+++ compose/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/layout/preference/section/PreferenceSectionInteractor.kt
@@ -1,9 +1,11 @@
 package com.bselzer.ktx.compose.ui.layout.preference.section
 
+import androidx.compose.ui.graphics.painter.Painter
 import com.bselzer.ktx.compose.ui.layout.image.ImageInteractor
 import com.bselzer.ktx.compose.ui.layout.modifier.interactable.InteractableModifier
 import com.bselzer.ktx.compose.ui.layout.project.Interactor
 import com.bselzer.ktx.compose.ui.layout.text.TextInteractor
+import com.bselzer.ktx.compose.ui.layout.text.textInteractor
 
 data class PreferenceSectionInteractor(
     override val modifier: InteractableModifier = InteractableModifier,
@@ -17,4 +19,16 @@ data class PreferenceSectionInteractor(
      * The [Interactor] for the title describing the section content.
      */
     val title: TextInteractor
-) : Interactor(modifier)
+) : Interactor(modifier) {
+    constructor(
+        title: String,
+        painter: Painter,
+        contentDescription: String? = null
+    ) : this(
+        image = ImageInteractor(
+            painter = painter,
+            contentDescription = contentDescription
+        ),
+        title = title.textInteractor()
+    )
+}
