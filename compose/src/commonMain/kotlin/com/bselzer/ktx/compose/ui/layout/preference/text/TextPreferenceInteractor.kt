9ee00a579f61d1572b698fad47e2b12513740906
Woody230 <bselzer1@outlook.com>
2022-06-02 20:17:56 -0400

    TextPreference alt

--- compose/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/layout/preference/text/TextPreferenceInteractor.kt
+++ compose/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/layout/preference/text/TextPreferenceInteractor.kt
@@ -1,9 +1,11 @@
 package com.bselzer.ktx.compose.ui.layout.preference.text
 
+import androidx.compose.ui.graphics.painter.Painter
 import com.bselzer.ktx.compose.ui.layout.image.ImageInteractor
 import com.bselzer.ktx.compose.ui.layout.modifier.interactable.InteractableModifier
 import com.bselzer.ktx.compose.ui.layout.project.Interactor
 import com.bselzer.ktx.compose.ui.layout.text.TextInteractor
+import com.bselzer.ktx.compose.ui.layout.text.textInteractor
 
 data class TextPreferenceInteractor(
     override val modifier: InteractableModifier = InteractableModifier,
@@ -22,4 +24,15 @@ data class TextPreferenceInteractor(
      * The [Interactor] for the subtitle.
      */
     val subtitle: TextInteractor
-) : Interactor(modifier)
+) : Interactor(modifier) {
+    constructor(
+        title: String,
+        subtitle: String,
+        painter: Painter,
+        contentDescription: String? = null
+    ) : this(
+        image = ImageInteractor(painter = painter, contentDescription = contentDescription),
+        title = title.textInteractor(),
+        subtitle = subtitle.textInteractor()
+    )
+}
