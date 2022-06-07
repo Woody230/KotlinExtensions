c0bb94e5127c9d76d0326a1171bc1f89ea7b475f
Woody230 <bselzer1@outlook.com>
2022-06-07 08:22:48 -0400

    DateTimeFormatter getter

--- datetime/src/androidMain/kotlin/com/bselzer/ktx/datetime/format/FormatStyleDateTimeFormatter.kt
+++ datetime/src/androidMain/kotlin/com/bselzer/ktx/datetime/format/FormatStyleDateTimeFormatter.kt
@@ -3,14 +3,16 @@ package com.bselzer.ktx.datetime.format
 import java.time.format.DateTimeFormatter
 
 actual class FormatStyleDateTimeFormatter actual constructor(
-    dateStyle: FormatStyle?, timeStyle: FormatStyle?
+    private val dateStyle: FormatStyle?,
+    private val timeStyle: FormatStyle?
 ) : com.bselzer.ktx.datetime.format.DateTimeFormatter, BaseDateTimeFormatter() {
-    override val formatter: DateTimeFormatter = when {
-        dateStyle != null && timeStyle != null -> DateTimeFormatter.ofLocalizedDateTime(dateStyle.asJavaFormatStyle(), timeStyle.asJavaFormatStyle())
-        dateStyle != null -> DateTimeFormatter.ofLocalizedDate(dateStyle.asJavaFormatStyle())
-        timeStyle != null -> DateTimeFormatter.ofLocalizedTime(timeStyle.asJavaFormatStyle())
-        else -> DateTimeFormatter.ofLocalizedDateTime(java.time.format.FormatStyle.FULL)
-    }
+    override val formatter: DateTimeFormatter
+        get() = when {
+            dateStyle != null && timeStyle != null -> DateTimeFormatter.ofLocalizedDateTime(dateStyle.asJavaFormatStyle(), timeStyle.asJavaFormatStyle())
+            dateStyle != null -> DateTimeFormatter.ofLocalizedDate(dateStyle.asJavaFormatStyle())
+            timeStyle != null -> DateTimeFormatter.ofLocalizedTime(timeStyle.asJavaFormatStyle())
+            else -> DateTimeFormatter.ofLocalizedDateTime(java.time.format.FormatStyle.FULL)
+        }
 
     private fun FormatStyle.asJavaFormatStyle() = when (this) {
         FormatStyle.FULL -> java.time.format.FormatStyle.FULL
--- datetime/src/androidMain/kotlin/com/bselzer/ktx/datetime/format/PatternDateTimeFormatter.kt
+++ datetime/src/androidMain/kotlin/com/bselzer/ktx/datetime/format/PatternDateTimeFormatter.kt
@@ -2,6 +2,9 @@ package com.bselzer.ktx.datetime.format
 
 import java.time.format.DateTimeFormatter
 
-actual class PatternDateTimeFormatter actual constructor(pattern: String) : com.bselzer.ktx.datetime.format.DateTimeFormatter, BaseDateTimeFormatter() {
-    override val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern(pattern)
+actual class PatternDateTimeFormatter actual constructor(
+    private val pattern: String
+) : com.bselzer.ktx.datetime.format.DateTimeFormatter, BaseDateTimeFormatter() {
+    override val formatter: DateTimeFormatter
+        get() = DateTimeFormatter.ofPattern(pattern)
 }
