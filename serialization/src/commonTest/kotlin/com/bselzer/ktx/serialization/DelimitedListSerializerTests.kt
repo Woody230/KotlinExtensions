4470149a127e96fec00103e38bd17727e154c927
Woody230 <bselzer1@outlook.com>
2022-06-26 13:21:06 -0400

    Restructure test to use config

--- serialization/src/commonTest/kotlin/com/bselzer/ktx/serialization/DelimitedListSerializerTests.kt
+++ serialization/src/commonTest/kotlin/com/bselzer/ktx/serialization/DelimitedListSerializerTests.kt
@@ -4,12 +4,8 @@ import com.bselzer.ktx.serialization.json.JsonContext
 import com.bselzer.ktx.serialization.serializer.DelimitedListSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonPrimitive
-import kotlinx.serialization.json.jsonPrimitive
 import nl.adaptivity.xmlutil.serialization.XML
 import nl.adaptivity.xmlutil.serialization.XmlSerialName
 import kotlin.test.Test
@@ -33,34 +29,34 @@ class DelimitedListSerializerTests {
     private data class Configuration(
         @Serializable(with = DelimitedListSerializer::class)
         @XmlSerialName(value = "enums", namespace = "", prefix = "")
-        val enums: List<TestEnum> = emptyList()
+        val enums: List<TestEnum> = emptyList(),
+
+        @Serializable(with = DelimitedListSerializer::class)
+        @XmlSerialName(value = "ints", namespace = "", prefix = "")
+        val ints: List<Int> = emptyList()
     )
 
     private val xml = XML {}
 
     @Test
-    fun enums() {
-        val delimited = "Bar|Foo|Baz"
-        val strings = listOf("Bar", "Foo", "Baz")
+    fun config() {
         val enums = listOf(TestEnum.B, TestEnum.A, TestEnum.C)
-        val configXml = "<Configuration enums=\"$delimited\"/>"
-        val config = Configuration(enums = enums)
+        val encodedEnums = listOf("Bar", "Foo", "Baz")
+        val delimitedEnums = "Bar|Foo|Baz"
+
+        val ints = listOf(65, 10295, 5109)
+        val encodedInts = listOf("65", "10295", "5109")
+        val delimitedInts = "65|10295|5109"
+
+        val configXml = "<Configuration enums=\"$delimitedEnums\" ints=\"$delimitedInts\"/>"
+        val config = Configuration(enums = enums, ints = ints)
 
         with(JsonContext) {
-            assertEquals(enums, strings.decode())
+            assertEquals(enums, encodedEnums.decode())
+            assertEquals(ints, encodedInts.decode())
         }
 
         assertEquals(config, xml.decodeFromString(configXml))
         assertEquals(configXml, xml.encodeToString(config))
-    }
-
-    @Test
-    fun ints() {
-        val delimited = "65|10295|5109"
-        val ints = listOf(65, 10295, 5109)
-        val serializer = DelimitedListSerializer(Int.serializer())
-
-        assertEquals(delimited, Json.encodeToJsonElement(serializer, ints).jsonPrimitive.content)
-        assertEquals(ints, Json.decodeFromJsonElement(serializer, JsonPrimitive(delimited)))
     }
 }
