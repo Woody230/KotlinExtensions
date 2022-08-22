c468ef2818f2826d5594f80e218d348b4b5cb03b
Woody230 <bselzer1@outlook.com>
2022-08-21 20:47:15 -0400

    Move connectivity related class to its own module

---
+++ ktor-client-connectivity/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+}
+
+publishing.publish(
+    project = project,
+    description = "Determine what connection capabilities exist using Ktor."
+)
+
+android.setup()
+
+dependencies {
+    testImplementation(project(mapOf("path" to ":intent")))
+}
+
+kotlin.setup {
+    commonMain {
+        ktorClient()
+        projectIntent()
+    }
+    commonTest {
+        coroutine()
+        ktorMockEngine()
+    }
+    androidTest {
+        ktorOkHttpEngine()
+    }
+    jvmTest {
+        ktorOkHttpEngine()
+    }
+}
