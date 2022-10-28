364dadb43001efd616379dabe9403c0789a11c4b
Woody230 <bselzer1@outlook.com>
2022-10-27 20:04:31 -0400

    reorder flow fields

--- openapi-model/src/commonMain/kotlin/com/bselzer/ktx/openapi/model/security/flow/AuthorizationCodeOAuthFlow.kt
+++ openapi-model/src/commonMain/kotlin/com/bselzer/ktx/openapi/model/security/flow/AuthorizationCodeOAuthFlow.kt
@@ -1,8 +1,13 @@
 package com.bselzer.ktx.openapi.model.security.flow
 
 import com.bselzer.ktx.openapi.model.OpenApiExtensions
+import com.bselzer.ktx.openapi.model.security.scheme.OpenApiSecurityScope
 
 data class AuthorizationCodeOAuthFlow(
+    override val refreshUrl: String? = null,
+    override val scopes: Map<OpenApiSecurityScope, String> = emptyMap(),
+    override val extensions: OpenApiExtensions = emptyMap(),
+
     /**
      * REQUIRED. The authorization URL to be used for this flow. This MUST be in the form of a URL. The OAuth2 standard requires the use of TLS.
      */
@@ -12,8 +17,4 @@ data class AuthorizationCodeOAuthFlow(
      * REQUIRED. The token URL to be used for this flow. This MUST be in the form of a URL. The OAuth2 standard requires the use of TLS.
      */
     val tokenUrl: String,
-
-    override val refreshUrl: String? = null,
-    override val scopes: Map<String, String> = emptyMap(),
-    override val extensions: OpenApiExtensions = emptyMap()
 ) : OAuthFlow
--- openapi-model/src/commonMain/kotlin/com/bselzer/ktx/openapi/model/security/flow/ImplicitOAuthFlow.kt
+++ openapi-model/src/commonMain/kotlin/com/bselzer/ktx/openapi/model/security/flow/ImplicitOAuthFlow.kt
@@ -1,14 +1,15 @@
 package com.bselzer.ktx.openapi.model.security.flow
 
 import com.bselzer.ktx.openapi.model.OpenApiExtensions
+import com.bselzer.ktx.openapi.model.security.scheme.OpenApiSecurityScope
 
 data class ImplicitOAuthFlow(
+    override val refreshUrl: String? = null,
+    override val scopes: Map<OpenApiSecurityScope, String> = emptyMap(),
+    override val extensions: OpenApiExtensions = emptyMap(),
+
     /**
      * REQUIRED. The authorization URL to be used for this flow. This MUST be in the form of a URL. The OAuth2 standard requires the use of TLS.
      */
     val authorizationUrl: String,
-
-    override val refreshUrl: String? = null,
-    override val scopes: Map<String, String> = emptyMap(),
-    override val extensions: OpenApiExtensions = emptyMap()
 ) : OAuthFlow
