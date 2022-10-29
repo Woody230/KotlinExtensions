00792286d9c96eb102c31eb2d0602c84c697ad8c
Woody230 <bselzer1@outlook.com>
2022-10-29 19:54:24 -0400

    TODO

--- openapi-model/src/commonMain/kotlin/com/bselzer/ktx/openapi/model/OpenApiAnyOrRuntimeExpression.kt
+++ openapi-model/src/commonMain/kotlin/com/bselzer/ktx/openapi/model/OpenApiAnyOrRuntimeExpression.kt
@@ -1,5 +1,6 @@
 package com.bselzer.ktx.openapi.model
 
+// TODO any or expression
 data class OpenApiAnyOrRuntimeExpression(
     val value: Any? = null,
     val expression: OpenApiRuntimeExpression? = null
