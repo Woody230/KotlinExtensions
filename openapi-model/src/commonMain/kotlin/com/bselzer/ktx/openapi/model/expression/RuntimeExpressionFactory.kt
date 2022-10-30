15b5ff3dfaa15382b86a54fbb4cc3dff22adba59
Woody230 <bselzer1@outlook.com>
2022-10-30 16:27:29 -0400

    runtime expression factory

--- openapi-model/src/commonMain/kotlin/com/bselzer/ktx/openapi/model/expression/RuntimeExpressionFactory.kt
+++ openapi-model/src/commonMain/kotlin/com/bselzer/ktx/openapi/model/expression/RuntimeExpressionFactory.kt
@@ -1,12 +1,14 @@
 package com.bselzer.ktx.openapi.model.expression
 
-class RuntimeExpressionFactory(value: String) : OpenApiRuntimeExpression by when {
-    value.isBlank() -> UnknownRuntimeExpression(value)
-    !value.startsWith('$') -> CompositeRuntimeExpression(value)
-    value == UrlRuntimeExpression.value -> UrlRuntimeExpression
-    value == MethodRuntimeExpression.value -> MethodRuntimeExpression
-    value == StatusCodeRuntimeExpression.value -> StatusCodeRuntimeExpression
-    value.startsWith("\$request.") -> RequestRuntimeExpression(value)
-    value.startsWith("\$response.") -> ResponseRuntimeExpression(value)
-    else -> UnknownRuntimeExpression(value)
+class RuntimeExpressionFactory(value: String) {
+    val expression: OpenApiRuntimeExpression = when {
+        value.isBlank() -> UnknownRuntimeExpression(value)
+        !value.startsWith('$') -> CompositeRuntimeExpression(value)
+        value == UrlRuntimeExpression.value -> UrlRuntimeExpression
+        value == MethodRuntimeExpression.value -> MethodRuntimeExpression
+        value == StatusCodeRuntimeExpression.value -> StatusCodeRuntimeExpression
+        value.startsWith("\$request.") -> RequestRuntimeExpression(value)
+        value.startsWith("\$response.") -> ResponseRuntimeExpression(value)
+        else -> UnknownRuntimeExpression(value)
+    }
 }
