99ef3ffbda365b2761e58095fc4d9b6280257a9a
Woody230 <bselzer1@outlook.com>
2022-11-06 11:37:10 -0500

    restructure

--- openapi-model/src/commonMain/kotlin/com/bselzer/ktx/openapi/model/OpenApiExampleValue.kt
+++ openapi-model/src/commonMain/kotlin/com/bselzer/ktx/openapi/model/OpenApiExampleValue.kt
@@ -1,6 +1,7 @@
 package com.bselzer.ktx.openapi.model
 
+import com.bselzer.ktx.openapi.model.value.OpenApiValue
 import kotlin.jvm.JvmInline
 
 @JvmInline
-value class OpenApiExampleValue(val value: Any)
+value class OpenApiExampleValue(val value: OpenApiValue)
