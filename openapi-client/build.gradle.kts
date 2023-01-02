2b30af065e84e5bd3b0701c94e1e079db2cf850c
Woody230 <bselzer1@outlook.com>
2023-01-02 05:50:06 -0500

    add openapi client module

---
+++ openapi-client/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+}
+
+publishing.publish(
+    project = project,
+    description = "OpenApi client model generation."
+)
+
+android.setup()
+
+kotlin.setup {
+    commonMain {
+        projectOpenApiModel()
+    }
+    commonTest()
+}
