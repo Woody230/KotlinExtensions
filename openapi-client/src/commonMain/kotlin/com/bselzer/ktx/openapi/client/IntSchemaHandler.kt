646c0973bf3cc242f6fb0f2d08e8e85c42eeadd0
Woody230 <bselzer1@outlook.com>
2023-01-03 22:45:48 -0500

    primitive schema handlers

--- openapi-client/src/commonMain/kotlin/com/bselzer/ktx/openapi/client/IntSchemaHandler.kt
+++ openapi-client/src/commonMain/kotlin/com/bselzer/ktx/openapi/client/IntSchemaHandler.kt
@@ -5,5 +5,5 @@ import com.bselzer.ktx.openapi.model.schema.OpenApiSchemaType
 object IntSchemaHandler : PrimitiveSchemaHandler() {
     override val className: ClassName = ClassName.INT
     override val formats: Collection<String?> = setOf(null, "int32")
-    override val type: Collection<OpenApiSchemaType> = setOf(OpenApiSchemaType.INTEGER)
+    override val types: Collection<OpenApiSchemaType> = setOf(OpenApiSchemaType.INTEGER)
 }
