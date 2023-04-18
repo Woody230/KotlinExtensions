6a681a4c755657bcbe95fcaf8e57310426bfdbb6
Woody230 <bselzer1@outlook.com>
2023-04-17 20:31:58 -0400

    cleanup

--- codegen-model/src/commonMain/kotlin/com/bselzer/ktx/codegen/model/annotation/SimpleAnnotation.kt
+++
@@ -1,9 +0,0 @@
-package com.bselzer.ktx.codegen.model.annotation
-
-import com.bselzer.ktx.codegen.model.type.name.ClassName
-
-data class SimpleAnnotation(
-    override val className: ClassName,
-    override val members: Collection<AnnotationMember> = emptyList(),
-    override val siteTarget: AnnotationSiteTarget? = null,
-) : Annotation
--- codegen-model/src/commonMain/kotlin/com/bselzer/ktx/codegen/model/file/import/SimpleImport.kt
+++
@@ -1,6 +0,0 @@
-package com.bselzer.ktx.codegen.model.file.import
-
-data class SimpleImport(
-    override val name: String,
-    override val alias: String? = null
-) : Import
