aaed18eec8f9e750ec3b1570e56a2e2f49599cf4
Woody230 <bselzer1@outlook.com>
2023-04-17 21:08:17 -0400

    cleanup

--- codegen-model/src/commonMain/kotlin/com/bselzer/ktx/codegen/model/type/class/enum/SimpleEnumClassConstructor.kt
+++
@@ -1,9 +0,0 @@
-package com.bselzer.ktx.codegen.model.type.`class`.enum
-
-import com.bselzer.ktx.codegen.model.codeblock.CodeBlock
-import com.bselzer.ktx.codegen.model.parameter.Parameter
-
-data class SimpleEnumClassConstructor(
-    override val parameters: Collection<Parameter>,
-    override val body: CodeBlock? = null
-) : EnumClassConstructor
