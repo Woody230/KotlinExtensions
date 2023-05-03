e0dd0a21593b18443c02d52e36b7696953c6dd4c
Woody230 <bselzer1@outlook.com>
2023-05-03 07:16:22 -0400

    restructure

--- buildSrc/src/main/kotlin/LocalProperty.kt
+++ buildSrc/src/main/kotlin/LocalProperty.kt
@@ -1,6 +1,14 @@
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 import org.gradle.api.Project
 
+object LocalProperty {
+    const val SONATYPE_USERNAME = "sonatype.username"
+    const val SONATYPE_PASSWORD = "sonatype.password"
+    const val SIGNING_KEY_ID = "signing.keyId"
+    const val SIGNING_KEY = "signing.key"
+    const val SIGNING_PASSWORD = "signing.password"
+}
+
 fun Project.hasLocalProperties(vararg names: String) = names.all { name -> hasLocalProperty(name) }
 fun Project.hasLocalProperty(name: String): Boolean = localPropertyOrNull(name) != null
 fun Project.localProperty(name: String): String = localPropertyOrNull(name) ?: throw NotImplementedError("Set the $name in local properties.")
---
+++ buildSrc/src/main/kotlin/Metadata.kt
@@ -0,0 +1,9 @@
+object Metadata {
+    const val GROUP_ID = "io.github.woody230"
+    const val SUBGROUP_ID = "ktx"
+    const val NAMESPACE_ID = "com.bselzer"
+    const val VERSION = "5.4.0"
+    const val JVM_TARGET = "11"
+    const val COMPILE_SDK = 33
+    const val MIN_SDK = 21
+}
