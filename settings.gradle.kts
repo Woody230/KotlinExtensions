ec48fc3b25bbdd41b549ac39ad09a0b53ece0268
Woody230 <bselzer1@outlook.com>
2023-05-06 14:25:58 -0400

    add toolchain

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.4.0")
+}
+
 // TODO feature preview https://docs.gradle.org/8.1.1/userguide/declaring_dependencies.html#sec:type-safe-project-accessors
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
