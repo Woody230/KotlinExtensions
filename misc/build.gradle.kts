4e439c4c6ab534f68b3aea48a07e268d8bdeecbb
Brandon Selzer <bselzer1@outlook.com>
2024-09-01 06:38:17 -0400

    update moko resources

--- misc/build.gradle.kts
+++ misc/build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     dependencies {
         classpath(libs.woody230.gradle.internal.android.desugar.plugin)
-        classpath(libs.woody230.gradle.internal.moko.resources.plugin)
+        classpath(libs.moko.resources.generator)
     }
 }
