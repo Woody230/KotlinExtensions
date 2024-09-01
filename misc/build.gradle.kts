05983f8e5d816d134a73c110eee3affc9a04c140
Brandon Selzer <bselzer1@outlook.com>
2024-09-01 07:36:08 -0400

    kotlin dsl classpath

--- misc/build.gradle.kts
+++ misc/build.gradle.kts
@@ -1,6 +1,7 @@
 buildscript {
     dependencies {
         classpath(libs.woody230.gradle.internal.android.desugar.plugin)
+        classpath(libs.kotlin.dsl.plugin)
         classpath(libs.moko.resources.generator)
     }
 }
