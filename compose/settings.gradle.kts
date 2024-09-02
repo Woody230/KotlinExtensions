50901f7b1ff6211ad09b857e098386238496cbe8
Brandon Selzer <bselzer1@outlook.com>
2024-09-02 11:25:50 -0400

    update bundled version

--- compose/settings.gradle.kts
+++ compose/settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 }
 
 plugins {
-    id("io.github.woody230.gradle.internal.bundled") version "1.4.0"
+    id("io.github.woody230.gradle.internal.bundled") version "1.5.0"
 }
 
 include("aboutlibraries")
