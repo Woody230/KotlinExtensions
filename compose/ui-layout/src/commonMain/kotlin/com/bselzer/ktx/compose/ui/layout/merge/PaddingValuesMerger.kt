b91d3cbeb6fb1f7988224efbc4e36da8f87d799a
Woody230 <bselzer1@outlook.com>
2024-11-14 11:36:40 -0500

    comment

--- compose/ui-layout/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/layout/merge/PaddingValuesMerger.kt
+++ compose/ui-layout/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/layout/merge/PaddingValuesMerger.kt
@@ -8,6 +8,7 @@ class PaddingValuesMerger : ComponentMerger<PaddingValues> {
     override val default: PaddingValues = Default
 
     companion object {
+        // NOTE can't use unspecified dp because there is a check on init
         @Stable
         val Default = PaddingValues(all = 0.dp)
     }
