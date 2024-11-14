433e2c810283c74bec3ff5c92e8e48fb94afbd27
Woody230 <bselzer1@outlook.com>
2024-11-14 10:11:25 -0500

    padding values => use 0.dp instead of unspecified due to new checks

--- compose/ui-layout/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/layout/merge/PaddingValuesMerger.kt
+++ compose/ui-layout/src/commonMain/kotlin/com/bselzer/ktx/compose/ui/layout/merge/PaddingValuesMerger.kt
@@ -2,13 +2,13 @@ package com.bselzer.ktx.compose.ui.layout.merge
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Stable
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 class PaddingValuesMerger : ComponentMerger<PaddingValues> {
     override val default: PaddingValues = Default
 
     companion object {
         @Stable
-        val Default = PaddingValues(all = Dp.Unspecified)
+        val Default = PaddingValues(all = 0.dp)
     }
 }
