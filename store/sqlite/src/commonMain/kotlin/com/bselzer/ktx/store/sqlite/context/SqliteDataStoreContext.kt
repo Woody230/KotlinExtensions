52045cfe5f8148805bb8d97414437bb207b7e65d
Brandon Selzer <bselzer1@outlook.com>
2024-11-30 09:12:57 -0500

    data store context as class instead of interface

--- store/sqlite/src/commonMain/kotlin/com/bselzer/ktx/store/sqlite/context/SqliteDataStoreContext.kt
+++ store/sqlite/src/commonMain/kotlin/com/bselzer/ktx/store/sqlite/context/SqliteDataStoreContext.kt
@@ -1,6 +1,6 @@
 package com.bselzer.ktx.store.sqlite.context
 
-interface SqliteDataStoreContext {
-    val file: SqliteFileContext
-    val platform: SqlitePlatformContext
-}
+class SqliteDataStoreContext(
+    internal val file: SqliteFileContext,
+    internal val platform: SqlitePlatformContext
+)
