f40935ce6aae9606d28117fb2f6a446c7e5fe94b
Brandon Selzer <bselzer1@outlook.com>
2026-03-07 15:05:54 -0500

    internal constructor

--- store/sqlite/src/commonMain/kotlin/com/bselzer/ktx/store/sqlite/context/SqliteDataStoreContext.kt
+++ store/sqlite/src/commonMain/kotlin/com/bselzer/ktx/store/sqlite/context/SqliteDataStoreContext.kt
@@ -1,6 +1,6 @@
 package com.bselzer.ktx.store.sqlite.context
 
-class SqliteDataStoreContext(
+class SqliteDataStoreContext internal constructor(
     internal val file: SqliteFileContext,
     internal val platform: SqlitePlatformContext
 )
