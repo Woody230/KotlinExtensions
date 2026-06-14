016e34922fb8fef4bbc2b4b3db3dd2df0a38744d
Brandon Selzer <bselzer1@outlook.com>
2026-06-14 07:22:23 -0400

    publish to central portal

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ pluginManagement {
 }
 
 plugins {
-    id("io.github.woody230.gradle.internal.bundled") version "1.7.0"
+    id("io.github.woody230.gradle.internal.bundled") version "1.8.0"
 }
 
 includeBuild("convention-plugins")
