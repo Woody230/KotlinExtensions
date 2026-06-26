b57c4ab611ad631b4c5978b1c527532c17fee31b
Brandon Selzer <bselzer1@outlook.com>
2026-06-25 21:05:26 -0400

    source/target compatibility 21

--- misc/datetime/src/jvmTest/kotlin/FormatStyleDateTimeFormatterTests.kt
+++ misc/datetime/src/jvmTest/kotlin/FormatStyleDateTimeFormatterTests.kt
@@ -12,12 +12,12 @@ class FormatStyleDateTimeFormatterTests {
         val formatter = FormatStyleDateTimeFormatter(FormatStyle.SHORT, FormatStyle.SHORT)
 
         TimeZone.setDefault(TimeZone.getTimeZone("Etc/GMT+12"))
-        assertEquals("5/11/22, 11:10 PM", formatter.format(instant))
+        assertEquals("5/11/22, 11:10\u202FPM", formatter.format(instant))
 
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Dacca"))
-        assertEquals("5/12/22, 5:10 PM", formatter.format(instant))
+        assertEquals("5/12/22, 5:10\u202FPM", formatter.format(instant))
 
         TimeZone.setDefault(TimeZone.getTimeZone("Pacific/Kiritimati"))
-        assertEquals("5/13/22, 1:10 AM", formatter.format(instant))
+        assertEquals("5/13/22, 1:10\u202FAM", formatter.format(instant))
     }
 }
