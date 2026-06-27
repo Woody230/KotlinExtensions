d55f1d455bab12e3bd128cfc637be2c4dcdbda52
Brandon Selzer <bselzer1@outlook.com>
2026-06-27 09:00:40 -0400

    fix compose namespace

--- convention-plugins/src/main/kotlin/io/github/woody230/ktx/convention/compose.gradle.kts
+++ convention-plugins/src/main/kotlin/io/github/woody230/ktx/convention/compose.gradle.kts
@@ -1,10 +1,16 @@
 package io.github.woody230.ktx.convention
 
+import com.bselzer.gradle.internal.android.kotlin.multiplatform.library.plugin.multiplatformAndroidLibraryExtension
+
 // TODO can't access libs from precompiled scripts https://github.com/gradle/gradle/issues/15383
 plugins {
     id("io.github.woody230.ktx.convention.multiplatform")
     id("io.github.woody230.gradle.internal.multiplatform-compose")
     id("io.github.woody230.gradle.internal.multiplatform-compose-test")
+}
+
+multiplatformAndroidLibraryExtension {
+    namespace.module.set("compose.$name")
 }
 
 multiplatformPublishExtension {
