a64d6c01c0355ea9a55b1bb9799f1de38b52a1d8
Brandon Selzer <bselzer1@outlook.com>
2026-06-27 07:43:55 -0400

    androidx.compose.ui.test.junit4.v2.createComposeRule

--- compose/constraint-layout/src/androidHostTest/kotlin/androidx/constraintlayout/compose/ConstraintLayoutTest.kt
+++ compose/constraint-layout/src/androidHostTest/kotlin/androidx/constraintlayout/compose/ConstraintLayoutTest.kt
@@ -27,7 +27,7 @@ import androidx.compose.ui.platform.InspectableValue
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.platform.ValueElement
 import androidx.compose.ui.platform.isDebugInspectorInfoEnabled
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.unit.*
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
